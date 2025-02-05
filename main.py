1a9ce61c116d34e8a997844d4dbc77392b2e84a9
Gabe <gabetest.py@gmail.com>
2025-02-05 21:12:37 +0100

    add getData to enable downloading datasets from GitHub

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from modules.uploadData import *
+from modules.getData import *
 
-zip_photos("goose_ducks_labeled")
+# zip_photos("goose_ducks_labeled")
+
+get_data_from_github("goose_ducks_labeled")
