59698e24bfa7540a8cd1ba0578769fb516a0b5a9
Gabe <g.francke.tech@gmail.com>
2025-03-05 22:21:43 +0100

    add split on test/train

--- modules/uploadData.py
+++ modules/uploadData.py
@@ -1,8 +1,9 @@
 import os
 import zipfile
+import random
 from pathlib import Path
 from PIL import Image
-from modules.utils import prepare_directory
+from modules.utils import prepare_directory, remove_directory
 
 def resize_image(image_path: Path, output_size=(64, 64)):
     with Image.open(image_path) as img:
@@ -14,37 +15,63 @@ def process_images(directory: Path, output_size=(64, 64)):
         if file.suffix.lower() in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]:
             resize_image(file, output_size)
 
-def get_all_image_files(directory: Path):
-    return [file for file in directory.rglob("*") if file.suffix.lower() in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]]
+def get_all_image_files(directory: Path, max_files=100):
+    all_files = [file for file in directory.rglob("*") if file.suffix.lower() in [".jpg", ".jpeg", ".png", ".gif", ".bmp"]]
+    return random.sample(all_files, min(len(all_files), max_files))
 
-def zip_photos(zip_name: str):
+def split_and_move_images(files, train_dir, test_dir, split_ratio=0.8):
+    random.shuffle(files)
+    split_index = int(len(files) * split_ratio)
+    
+    train_files = files[:split_index]
+    test_files = files[split_index:]
+    
+    for file in train_files:
+        category = file.parent.name
+        target_folder = train_dir / category
+        prepare_directory(target_folder)
+        file.rename(target_folder / file.name)
+
+    for file in test_files:
+        category = file.parent.name
+        target_folder = test_dir / category
+        prepare_directory(target_folder)
+        file.rename(target_folder / file.name)
+
+def zip_directory(source_folder: Path, data_path_for_zip: Path, zip_name: str):
+    zip_path = data_path_for_zip / f"{zip_name}.zip"
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for file in source_folder.rglob("*"):
+            zipf.write(file, file.relative_to(source_folder))
+    print(f"Zipped to: {zip_path}")
+
+def zip_data(zip_name: str):
     # Setup path to data folder
     data_path = Path("data/")
     data_path_for_zip = data_path / "zip"
-    source_folder = data_path / "photos_to_zip"
-    zip_path = data_path_for_zip / f"{zip_name}.zip"
-    label_1_path = source_folder / "ducks"
-    label_2_path = source_folder / "goose"
+    photos_source  = data_path / "photos_to_zip"
+    output_folder  = data_path_for_zip / zip_name
+    train_folder = output_folder / "train"
+    test_folder = output_folder / "test"
     
     # Prepare directories
     prepare_directory(data_path)
     prepare_directory(data_path_for_zip)
-    prepare_directory(source_folder)
-    prepare_directory(label_1_path)
-    prepare_directory(label_2_path)
+    prepare_directory(output_folder)
+    prepare_directory(train_folder)
+    prepare_directory(test_folder)
     
-    files_to_zip = get_all_image_files(source_folder)
+    files_to_process = get_all_image_files(photos_source, max_files=100)
     
-    if not any(label_1_path.iterdir()) and not any(label_2_path.iterdir()):
-        print("Brak zdjęć do spakowania. Kod zatrzymany.")
+    if not files_to_process:
+        print("Brak zdjęć do przetworzenia.")
         return
 
-    print("Resizing images...")
-    process_images(label_1_path, output_size=(64, 64))
-    process_images(label_2_path, output_size=(64, 64))
+    split_and_move_images(files_to_process, train_folder, test_folder)
 
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for file in files_to_zip:
-            zipf.write(file, file.relative_to(source_folder))
-    
-    print(f"Directory created: {zip_path}")
+    process_images(train_folder, output_size=(64, 64))
+    process_images(test_folder, output_size=(64, 64))
+
+    zip_directory(output_folder, data_path_for_zip, zip_name)
+
+    remove_directory(output_folder)
